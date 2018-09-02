ea63181a5781b069dd3629dc1ba022b5cb6688b6
wozaki <ozaki.shun5@gmail.com>
2018-09-02 21:17:44 +0900

    Delegate connection logic to Servers

--- servers.go
+++ servers.go
@@ -1,21 +1,38 @@
 package memgo
 
 import (
+	"net"
 	"stathat.com/c/consistent"
+	"time"
 )
 
 type Servers struct {
-	Hashing *consistent.Consistent
+	Hashing        *consistent.Consistent
+	ConnectTimeout time.Duration
 }
 
-func (d *Servers) Get(key string) (server string, err error) {
-	return d.Hashing.Get(key)
+func (s *Servers) Get(key string) (server string, err error) {
+	return s.Hashing.Get(key)
 }
 
-func NewServers(servers []string) Servers {
+func NewServers(servers []string, connectTimeout time.Duration) Servers {
 	c := consistent.New()
 	for _, d := range servers {
 		c.Add(d)
 	}
-	return Servers{Hashing: c}
+	return Servers{Hashing: c, ConnectTimeout: connectTimeout}
+}
+
+func (s *Servers) connect(key string) (net.Conn, error) {
+	server, err := s.Get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialTimeout("tcp", server, s.ConnectTimeout)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
